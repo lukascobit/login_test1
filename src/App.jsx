e711188b81b4b4bed005e7a9e38984068f668649
lukascobit <lukyn.odehnal333@gmail.com>
2021-08-16 09:02:56 +0200

    another aproach

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Home from './components/Home';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import { AuthProvider } from './context/authContext';
+import React, { useEffect, useState } from 'react'
+import firebase from 'firebase'
+import "firebase/firestore"
+import "firebase/auth"
+import Home from "./components/Home"
+import Signup from "./components/Signup"
+
+import {useAuthState} from "react-firebase-hooks/auth"
+import {useCollectionData} from "react-firebase-hooks/firestore"
+import { Simulate } from 'react-dom/test-utils'
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_I,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 
 function App() {
 
+  const [user] = useAuthState(auth)
+
   return (
     <div>
-      <Router>
-        <AuthProvider>
-          <Switch>
-              
-            <Route exact path="/">
-              <Signup/>
-            </Route>
-            
-          </Switch>
-        </AuthProvider>
-      </Router>
+      {user ? <Home/> : <Signup/>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
 import firebase from "firebase";
 import "firebase/auth"
 
-console.log(process.env);
-    
 
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN ,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
     projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_PROJECT_ID,
-    messagingSenderId: process.env.REACT_APP_STORAGE_BUCKET,
+    storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_SENDER_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 export const auth = app.auth()
